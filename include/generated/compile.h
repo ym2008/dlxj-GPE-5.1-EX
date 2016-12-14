25e72e8bb2d5e7433831acb0e361cfbd63ab1a82
Yin Ming <yinming2008@live.com>
2016-12-14 19:00:09 +0800

    dlxj-GPE-5.1-EX22.4

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Fri Dec 9 19:10:58 CST 2016"
+#define LINUX_COMPILE_BY "hasee"
+#define LINUX_COMPILE_HOST "hasee-QTJ5"
+#define LINUX_COMPILER "gcc version 4.9.1 20140529 (prerelease) (crosstool-NG linaro-1.13.1+bzr2672 - Linaro GCC 2014.05 - LaboDJ - Cortex-A15) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.10-ElementalX-YM2008-22.4-GPE"
